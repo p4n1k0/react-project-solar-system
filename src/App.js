65da145bb4ef7fccd4517a3b4de86be9c72a5cf1
Tarick <gabre.tarick@gmail.com>
2022-04-06 20:29:13 -0300

    requiriment 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Header from './components/Header';
 
-class App extends React.Component {
-  render() {
-    return (<p>Sistema Solar</p>);
-  }
+class App extends Component {
+  render() { return (<Header />) }
 }
 
 export default App;
