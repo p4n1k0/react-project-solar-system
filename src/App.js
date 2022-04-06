b2536dd3d30c5a7c726dc9726186a5717f02bb6b
Tarick <gabre.tarick@gmail.com>
2022-04-06 20:37:10 -0300

    lint

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import Header from './components/Header';
 
 class App extends Component {
-  render() { return (<Header />) }
+  render() { return (<Header />); }
 }
 
 export default App;
- 
