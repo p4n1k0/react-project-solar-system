e13fed64d154625d818e1db76433be846874b018
p4n1k0 <gabre.tarick@gmail.com>
2025-03-31 23:21:01 -0300

    renderiza uma lsita com as missoes espaciais

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        <section className="Missions">
+          {missions.map((e) => (
+            <MissionCard
+              name={e.name}
+              year={e.year}
+              country={e.country}
+              destination={e.destination}
+              key={e.name}
+            />
+          ))}
+        </section>
       </div>
     );
   }
